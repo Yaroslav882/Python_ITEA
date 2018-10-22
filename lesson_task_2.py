5d60050d352df41198fb744f0856cfa4cfd2527b
Yaroslav Nahnii <nagniy_ya@yahoo.com>
2018-10-22 15:23:22 +0300

    Add files via upload

--- lesson_task_2.py
+++ lesson_task_2.py
@@ -6,6 +6,23 @@ if your_name == my_name:
 else:
     print('Сорри дружище, другие именя не признаю')
 
+# 3. Решение уровнения ax^2 + bx + c = 0
+a = float(input("Введите значение a = "))
+b = float(input("Введите значение b = "))
+c = float(input("Введите значение c = "))
+discrim = b**2 - 4 * a * c
+print("Дискриминант D = %.2f" % discrim)
+if discrim > 0:
+	import math
+	x1 = (-b + math.sqrt(discrim)) / (2 * a)
+	x2 = (-b - math.sqrt(discrim)) / (2 * a)
+	print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif discrim == 0:
+	x = -b / (2 * a)
+	print("x = %.2f" % x)
+else:
+	print("Сорри, корней тут нет")
+
 # 4. Запросить у пользователя два числа
 numb_1 = int(input('Введите первое число - '))
 numb_2 =int(input('Введите второе число - '))
